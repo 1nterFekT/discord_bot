1d3acbdb3e83b2f82aabab842b7b8943bac01be9
lgnidk <legionidk@internet.ru>
2026-07-30 21:30:56 +0300

    events cog & updated logger

--- bot.py
+++ bot.py
@@ -1,31 +1,17 @@
 import os
-
 import disnake
 from disnake.ext import commands
-from dotenv import load_dotenv
-from utils.logger import setup_logger
 
-load_dotenv()
-
-TOKEN = os.getenv("TOKEN")
-
-logger = setup_logger()
+from config import STABLE_TOKEN, BETA_TOKEN
+from utils.logger import logger
 
 intents = disnake.Intents.default()
 intents.message_content = True
-
 bot = commands.InteractionBot(intents=intents)
 
-def load_cogs():
-    for filename in os.listdir("./cogs"):
-        if filename.endswith(".py") and filename != "__init__.py":
-            bot.load_extension(f"cogs.{filename[:-3]}")
-            logger.info(f"Загружен cog: {filename}")
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py") and filename != "__init__.py":
+        bot.load_extension(f"cogs.{filename[:-3]}")
+        logger.info(f"Загружен cog: {filename}")
 
-@bot.event
-async def on_ready():
-    logger.info(f"Бот запущен как {bot.user}") # можно использовать .info, .warning, .error
-
-load_cogs()
-
-bot.run(TOKEN)
+bot.run(STABLE_TOKEN)
--- utils/logger.py
+++ utils/logger.py
@@ -1,9 +1,12 @@
 import logging
 
+
 def setup_logger():
     logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s | %(levelname)s | %(message)s"
+        level=logging.INFO, format="%(asctime)s | %(levelname)s | %(message)s"
     )
 
     return logging.getLogger("discord_bot")
+
+
+logger = setup_logger()
